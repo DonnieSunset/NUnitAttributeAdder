a85bb3d995ceae86950d41e559b6375e7806ec80
Randall Flagg <flagg1980@outlook.com>
2018-04-21 07:56:27 +0200

    Initial Commit

---
+++ SampleTests/MyTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTests
+{
+    [TestFixture]
+    public class MyTestFixture
+    {
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
